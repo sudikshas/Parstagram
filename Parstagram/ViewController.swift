b6c9d3fd3660b7412ffe7a9435d0a261a96c6f70
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-02-25 22:32:55 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Sudiksha Sarvepalli on 2/25/20.
+//  Copyright © 2020 Sudiksha Sarvepalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
