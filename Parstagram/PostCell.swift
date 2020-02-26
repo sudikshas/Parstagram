4a2350fff912442045831f378abd66262befb65d
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2020-02-26 14:13:01 -0800

    updated Parstagram Part 1

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Sudiksha Sarvepalli on 2/26/20.
+//  Copyright © 2020 Sudiksha Sarvepalli. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
